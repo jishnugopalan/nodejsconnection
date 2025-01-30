a5aa30047aa78175ae47b7c1a58bc1d6fe091c72
Jishnu Gopalan K P <55798251+jishnugopalan@users.noreply.github.com>
2025-01-30 13:43:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("db connected");
+    })
+    .catch(err => {
+        console.error("db connection error:", err);
+    });
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.post('/login', (req, res) => {
+    res.send("Login Successful");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
